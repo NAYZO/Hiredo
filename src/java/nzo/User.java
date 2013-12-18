187cd5e7d07fb6214e467bda042581e7bbbb2dfd
nayzo <nayzo@nayzo-comp>
2013-12-18 11:48:07 +0100

    First Create

---
+++ src/java/nzo/User.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nzo;
+
+/**
+ *
+ * @author nayzo
+ */
+public class User {
+    private String login;
+    private String name;
+    
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
